77a8c8b4d7b51df4fb7e4561503eaa81c3e871f7
HugoW5 <hugo05.wakt@gmail.com>
2025-08-21 22:12:04 +0200

    Added ApiResponse model

---
+++ TooliRent/Repositories/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TooliRent.Repositories
+{
+	public class ApiResponse<T>
+	{
+		public bool IsError { get; set; } = false;
+		public string Message { get; set; } = string.Empty;
+		public T Data { get; set; } = default!;
+		public List<string> Errors { get; set; } = null!;
+	}
+}

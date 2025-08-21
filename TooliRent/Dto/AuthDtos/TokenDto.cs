0bb631f201c87b112f9754dcb1376d64992e6526
HugoW5 <hugo05.wakt@gmail.com>
2025-08-21 22:15:37 +0200

    Add TokenDto

---
+++ TooliRent/Dto/AuthDtos/TokenDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TooliRent.Dto.AuthDtos
+{
+	public class TokenDto
+	{
+		public string Token { get; set; } = string.Empty;
+		public string RefreshToken { get; set; } = string.Empty;
+	}
+}
--- TooliRent/Services/Interfaces/IAuthService.cs
+++ TooliRent/Services/Interfaces/IAuthService.cs
@@ -1,10 +1,11 @@
 ﻿using TooliRent.Dto.AuthDtos;
+using TooliRent.Repositories;
 
 namespace TooliRent.Services.Interfaces
 {
 	public interface IAuthService
 	{
-		Task<(string Token, string RefreshToken)> RegisterAsync(RegisterDto dto);
+		Task<ApiResponse<TokenDto>> RegisterAsync(RegisterDto dto);
 		Task<(string Token, string RefreshToken)> LoginAsync(LoginDto dto);
 		Task<(string Token, string RefreshToken)> RefreshTokenAsync(string refreshToken, CancellationToken ct = default);
 	}

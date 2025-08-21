10b9dd2f1de27d4ad254e080df949e9d10fc1605
HugoW5 <hugo05.wakt@gmail.com>
2025-08-21 14:55:16 +0200

    Changed IdentityUser to IdentityResult

--- TooliRent/Repositories/Interfaces/IAuthRepository.cs
+++ TooliRent/Repositories/Interfaces/IAuthRepository.cs
@@ -6,7 +6,7 @@ namespace TooliRent.Repositories.Interfaces
 	public interface IAuthRepository
 	{
 		Task<IdentityUser?> GetUserByEmailAsync(string email);
-		Task<IdentityUser> CreateUserAsync(IdentityUser user, string password);
+		Task<IdentityResult> CreateUserAsync(IdentityUser user, string password);
 		Task<bool> CheckPasswordAsync(IdentityUser user, string password);
 
 		Task AddRefreshTokenAsync(RefreshToken token);

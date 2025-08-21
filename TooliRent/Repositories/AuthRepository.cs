2affdd3b17fee35d5777f71617099f4fef0d6232
HugoW5 <hugo05.wakt@gmail.com>
2025-08-21 14:46:28 +0200

    Implemented GetUserByEmailAsync

---
+++ TooliRent/Repositories/AuthRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TooliRent.Data;
+using TooliRent.Models;
+using TooliRent.Repositories.Interfaces;
+
+namespace TooliRent.Repositories
+{
+	public class AuthRepository : IAuthRepository
+	{
+		private readonly UserManager<IdentityUser> _userManager;
+		private readonly ApplicationDbContext _context;
+
+		public AuthRepository(UserManager<IdentityUser> userManager, ApplicationDbContext context)
+		{
+			_userManager = userManager;
+			_context = context;
+		}
+
+		public Task AddRefreshTokenAsync(RefreshToken token)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> CheckPasswordAsync(IdentityUser user, string password)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IdentityUser> CreateUserAsync(IdentityUser user, string password)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<RefreshToken?> GetRefreshTokenAsync(string token)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IdentityUser?> GetUserByEmailAsync(string email)
+		{
+			return _userManager.FindByEmailAsync(email);
+		}
+
+		public Task UpdateRefreshTokenAsync(RefreshToken token)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

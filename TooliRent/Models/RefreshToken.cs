a04794dbaba3ad206e2f279f4825f76779aa9b43
HugoW5 <hugo05.wakt@gmail.com>
2025-08-21 12:35:12 +0200

    Added RefreshToken Model

---
+++ TooliRent/Models/RefreshToken.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TooliRent.Models
+{
+	public class RefreshToken
+	{
+		[Key]
+		public int Id { get; set; }
+		public string Token { get; set; } = string.Empty;
+		public string UserId { get; set; } = string.Empty;
+		public DateTime ExpiryDate { get; set; }
+		public bool IsUsed { get; set; }
+		public bool IsRevoked { get; set; }
+	}
+}

bf60431d56d17c0adfda99731a0994a08aa98124
HugoW5 <hugo05.wakt@gmail.com>
2025-08-21 14:42:23 +0200

    Added IAuthRepsitory

---
+++ TooliRent/Repositories/Interfaces/IAuthRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TooliRent.Models;
+
+namespace TooliRent.Repositories.Interfaces
+{
+	public interface IAuthRepository
+	{
+		Task<IdentityUser?> GetUserByEmailAsync(string email);
+		Task<IdentityUser> CreateUserAsync(IdentityUser user, string password);
+		Task<bool> CheckPasswordAsync(IdentityUser user, string password);
+
+		Task AddRefreshTokenAsync(RefreshToken token);
+		Task<RefreshToken?> GetRefreshTokenAsync(string token);
+		Task UpdateRefreshTokenAsync(RefreshToken token);
+	}
+}

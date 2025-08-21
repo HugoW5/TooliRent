147783894d2a852bceef2bf0b8b62870cacbac96
HugoW5 <hugo05.wakt@gmail.com>
2025-08-21 15:00:30 +0200

    Added CancellationTokens in the Auth Repository

--- TooliRent/Repositories/Interfaces/IAuthRepository.cs
+++ TooliRent/Repositories/Interfaces/IAuthRepository.cs
@@ -5,12 +5,11 @@ namespace TooliRent.Repositories.Interfaces
 {
 	public interface IAuthRepository
 	{
-		Task<IdentityUser?> GetUserByEmailAsync(string email);
-		Task<IdentityResult> CreateUserAsync(IdentityUser user, string password);
-		Task<bool> CheckPasswordAsync(IdentityUser user, string password);
-
-		Task AddRefreshTokenAsync(RefreshToken token);
-		Task<RefreshToken?> GetRefreshTokenAsync(string token);
-		Task UpdateRefreshTokenAsync(RefreshToken token);
+		Task<IdentityUser?> GetUserByEmailAsync(string email, CancellationToken ct = default);
+		Task<IdentityResult> CreateUserAsync(IdentityUser user, string password, CancellationToken ct = default);
+		Task<bool> CheckPasswordAsync(IdentityUser user, string password, CancellationToken ct = default);
+		Task AddRefreshTokenAsync(RefreshToken token, CancellationToken ct = default);
+		Task<RefreshToken?> GetRefreshTokenAsync(string token, CancellationToken ct = default);
+		Task UpdateRefreshTokenAsync(RefreshToken token, CancellationToken ct = default);
 	}
 }

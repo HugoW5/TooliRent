b4c884bf6cb784a1f3cdf8f4cc54cd4494e9aadf
HugoW5 <hugo05.wakt@gmail.com>
2025-08-24 13:43:08 +0200

    Add MsTest Project and TestInitialize

---
+++ Tests/AuthServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TooliRent.Repositories.Interfaces;
+using TooliRent.Services;
+using TooliRent.Services.Interfaces;
+
+namespace Tests
+{
+	[TestClass]
+	public class AuthServiceTests
+	{
+		private Mock<IRefreshTokenRepository> _tokenRepoMock = null!;
+		private Mock<ITokenService> _tokenServiceMock = null!;
+		private Mock<IConfiguration> _configMock = null!;
+		private UserManager<IdentityUser> _userManager = null!;
+		private AuthService _authService = null!;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			_tokenRepoMock = new Mock<IRefreshTokenRepository>();
+			_tokenServiceMock = new Mock<ITokenService>();
+			_configMock = new Mock<IConfiguration>();
+
+			// Mock IConfiguration["JwtSettings:RefreshTokenExpiryDays"]
+			_configMock.Setup(c => c["JwtSettings:RefreshTokenExpiryDays"]).Returns("7");
+
+			// Setup UserManager with in-memory store
+			var store = new Mock<IUserStore<IdentityUser>>();
+			_userManager = new UserManager<IdentityUser>(
+				store.Object, null!, null!, null!, null!, null!, null!, null!, null!
+			);
+
+			// Initialize AuthService
+			_authService = new AuthService(
+				_tokenRepoMock.Object,
+				_tokenServiceMock.Object,
+				_configMock.Object,
+				_userManager
+			);;
+		}
+	}
+}

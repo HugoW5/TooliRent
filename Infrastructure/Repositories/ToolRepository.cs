45e2cc1985b1424a971a4f1b1a3aa49981d32b8f
HugoW5 <hugo05.wakt@gmail.com>
2025-09-05 19:04:35 +0200

    Delete tool

--- Infrastructure/Repositories/ToolRepository.cs
+++ Infrastructure/Repositories/ToolRepository.cs
@@ -17,27 +17,13 @@ namespace Infrastructure.Repositories
 			await _context.Tools.AddAsync(tool);
 		}
 
-		public Task DeleteAsync(Tool tool)
-		{
-			throw new NotImplementedException();
-		}
-
 		public Task DeleteAsync(Tool tool, CancellationToken ct)
 		{
-			throw new NotImplementedException();
-		}
-
-		public Task<IEnumerable<Tool>> GetAllAsync()
-		{
-			throw new NotImplementedException();
+			_context.Tools.Remove(tool);
+			return Task.CompletedTask;
 		}
 
 		public Task<IEnumerable<Tool>> GetAllAsync(CancellationToken ct)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<IEnumerable<Tool>> GetAvailableAsync()
 		{
 			throw new NotImplementedException();
 		}
@@ -47,17 +33,7 @@ namespace Infrastructure.Repositories
 			throw new NotImplementedException();
 		}
 
-		public Task<IEnumerable<Tool>> GetByCategoryAsync(Guid categoryId)
-		{
-			throw new NotImplementedException();
-		}
-
 		public Task<IEnumerable<Tool>> GetByCategoryAsync(Guid categoryId, CancellationToken ct)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<Tool?> GetByIdAsync(Guid id)
 		{
 			throw new NotImplementedException();
 		}
@@ -67,17 +43,7 @@ namespace Infrastructure.Repositories
 			throw new NotImplementedException();
 		}
 
-		public Task<IEnumerable<Tool>> SearchByNameAsync(string name)
-		{
-			throw new NotImplementedException();
-		}
-
 		public Task<IEnumerable<Tool>> SearchByNameAsync(string name, CancellationToken ct)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task UpdateAsync(Tool tool)
 		{
 			throw new NotImplementedException();
 		}

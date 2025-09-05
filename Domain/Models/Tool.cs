ba6a4d7aada3207be2ce08c316f30a00bd81f203
HugoW5 <hugo05.wakt@gmail.com>
2025-09-05 12:27:34 +0200

    Add Tool class to Domain.Models namespace
    Introduced a new `Tool` class with properties for `Id`, `Name`, `Description`, `CategoryId`, `Category`, `Status`, and a collection of `BookingItems`. The `Name` and `Category` are non-nullable, `Description` is nullable, and `Status` defaults to `ToolStatus.Available`. `BookingItems` is initialized as an empty list.

---
+++ Domain/Models/Tool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+	public class Tool
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; } = null!;
+		public string? Description { get; set; }
+
+		public Guid CategoryId { get; set; }
+		public Category Category { get; set; } = null!;
+
+		public ToolStatus Status { get; set; } = ToolStatus.Available;
+		public ICollection<BookingItem> BookingItems { get; set; } = new List<BookingItem>();
+	}
+}

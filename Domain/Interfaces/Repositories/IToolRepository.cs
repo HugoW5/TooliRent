568f9e15e479820149528e383dba81b3902b87e3
HugoW5 <hugo05.wakt@gmail.com>
2025-09-05 18:56:56 +0200

    Added IToolRepository

---
+++ Domain/Interfaces/Repositories/IToolRepository.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories
+{
+	public interface IToolRepository
+	{
+		// Basic CRUD
+		Task<Tool?> GetByIdAsync(Guid id);
+		Task<IEnumerable<Tool>> GetAllAsync();
+		Task AddAsync(Tool tool);
+		Task UpdateAsync(Tool tool);
+		Task DeleteAsync(Tool tool);
+
+		// Domain-specific queries
+		Task<IEnumerable<Tool>> GetByCategoryAsync(Guid categoryId);
+		Task<IEnumerable<Tool>> GetAvailableAsync();
+		Task<IEnumerable<Tool>> SearchByNameAsync(string name);
+	}
+}

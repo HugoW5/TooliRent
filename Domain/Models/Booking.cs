bcb399abdc1bb0384f4b0f8ab01b0991da1f92ce
HugoW5 <hugo05.wakt@gmail.com>
2025-09-05 12:28:58 +0200

    Adde booking model

---
+++ Domain/Models/Booking.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+	public class Booking
+	{
+		public Guid Id { get; set; }
+
+		public string UserId { get; set; } = null!; // FK till IdentityUser
+		public IdentityUser User { get; set; } = null!;
+
+		public DateTime StartAt { get; set; }
+		public DateTime EndAt { get; set; }
+		public BookingStatus Status { get; set; } = BookingStatus.Reserved;
+
+		public ICollection<BookingItem> BookingItems { get; set; } = new List<BookingItem>();
+	}
+}

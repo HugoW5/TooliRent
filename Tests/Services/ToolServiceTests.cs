9ba9022a8c7ae69f9fc94ee6eb5d31929455b3e1
HugoW5 <hugo05.wakt@gmail.com>
2025-09-08 14:30:40 +0200

    Test for add tool

--- Tests/Services/ToolServiceTests.cs
+++ Tests/Services/ToolServiceTests.cs
@@ -1,6 +1,8 @@
-﻿using Application.Services;
+﻿using Application.Dto.ToolDtos;
+using Application.Services;
 using AutoMapper;
 using Domain.Interfaces.Repositories;
+using Domain.Models;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -27,6 +29,30 @@ namespace Tests.Services
 
 			_service = new ToolService(_repoMock.Object, _mapperMock.Object, _uowMock.Object);
 		}
+
+		[TestMethod]
+		public async Task AddAsync_ShouldReturnId_WhenCategoryExistsAndToolAdded()
+		{
+			// Arrange
+			var dto = new AddToolDto { CategoryId = Guid.NewGuid(), Name = "Hammer" };
+			var tool = new Tool { Id = Guid.NewGuid(), Name = "Hammer" };
+			var toolId = tool.Id;
+
+			_uowMock.Setup(u => u.CategoryExistsAsync(dto.CategoryId, It.IsAny<CancellationToken>()))
+				.ReturnsAsync(true); // Category does not exist but return true anyway
+
+			_mapperMock.Setup(m => m.Map<Tool>(dto)).Returns(tool);
+			_repoMock.Setup(r => r.AddAsync(tool, It.IsAny<CancellationToken>()))
+				.ReturnsAsync(toolId);
+
+			// Act
+			var result = await _service.AddAsync(dto);
+
+			// Assert
+			Assert.AreEqual(toolId, result);
+			_uowMock.Verify(u => u.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+		}
+
 
 	}
 }

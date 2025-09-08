dd635a40e4df2810393b35129347c2dca46f67fb
HugoW5 <hugo05.wakt@gmail.com>
2025-09-08 14:33:07 +0200

    Add tool should throw exception when repo return is null

--- Tests/Services/ToolServiceTests.cs
+++ Tests/Services/ToolServiceTests.cs
@@ -67,6 +67,22 @@ namespace Tests.Services
 			await _service.AddAsync(dto);
 		}
 
+		[TestMethod]
+		[ExpectedException(typeof(Exception))]
+		public async Task AddAsync_ShouldThrow_WhenRepositoryReturnsNull()
+		{
+			// Arrange
+			var dto = new AddToolDto { CategoryId = Guid.NewGuid(), Name = "Hammer" };
+			var tool = new Tool { Name = "Hammer" };
 
+			_uowMock.Setup(u => u.CategoryExistsAsync(dto.CategoryId, It.IsAny<CancellationToken>()))
+				.ReturnsAsync(true);
+			_mapperMock.Setup(m => m.Map<Tool>(dto)).Returns(tool);
+			_repoMock.Setup(r => r.AddAsync(tool, It.IsAny<CancellationToken>()))
+				.ReturnsAsync((Guid?)null);
+
+			// Act
+			await _service.AddAsync(dto);
+		}
 	}
 }

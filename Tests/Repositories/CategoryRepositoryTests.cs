5def2f9e59410f5f25c6661612fa725fc2f28e51
HugoW5 <hugo05.wakt@gmail.com>
2025-09-09 16:13:41 +0200

    Test for addning a new category and making sure it adds correctly

---
+++ Tests/Repositories/CategoryRepositoryTests.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Models;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Repositories
+{
+		[TestClass]
+	public class CategoryRepositoryTests
+	{
+		private ApplicationDbContext GetInMemoryDbContext()
+		{
+			var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+				.UseInMemoryDatabase(Guid.NewGuid().ToString()) // unique DB per test
+				.Options;
+
+			return new ApplicationDbContext(options);
+		}
+
+		[TestMethod]
+		public async Task AddCategory_Should_Add_Category_To_DbContext()
+		{
+			// Arrange
+			var ct = CancellationToken.None;
+			var context = GetInMemoryDbContext();
+			var repo = new CategoryRepository(context);
+
+			var category = new Category
+			{
+				Id = Guid.NewGuid(),
+				Name = "Hardware",
+				Description = "Hardware tools"
+			};
+
+			// Act
+			await repo.AddAsync(category, ct);
+			await context.SaveChangesAsync(ct);
+
+			// Assert
+			var savedCategory = await repo.GetByIdAsync(category.Id, ct);
+			Assert.IsNotNull(savedCategory);
+			Assert.AreEqual("Hardware", savedCategory.Name);
+			Assert.AreEqual("Hardware tools", savedCategory.Description);
+		}
+	}
+}

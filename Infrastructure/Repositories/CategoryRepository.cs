82f806744f694f6c7bfbb067cb0ab2c02b31ac1f
HugoW5 <hugo05.wakt@gmail.com>
2025-09-09 15:41:00 +0200

    Add new category async

---
+++ Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Interfaces.Repositories;
+using Domain.Models;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+	public class CategoryRepository : ICategoryRepository
+	{
+		private readonly ApplicationDbContext _context;
+
+		public CategoryRepository(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public async Task<Guid?> AddAsync(Category category, CancellationToken ct)
+		{
+			var addedCategory = await _context.Categories.AddAsync(category, ct);
+			return addedCategory.Entity.Id;
+		}
+
+	}
+}

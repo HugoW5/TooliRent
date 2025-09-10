5bb8996f8118997a4b32d8ae7bbc9e4ec44f2077
HugoW5 <hugo05.wakt@gmail.com>
2025-09-10 16:55:58 +0200

    Get all bookings

---
+++ TooliRent/Controllers/BookingsController.cs
@@ -0,0 +1,32 @@
+﻿using Application.Dto.BookingDtos;
+using Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Responses;
+
+namespace TooliRent.Controllers
+{
+	[Route("/api/[controller]")]
+	[ApiController]
+	public class BookingsController : Controller
+	{
+		private readonly IBookingService _bookingService;
+
+		public BookingsController(IBookingService service)
+		{
+			_bookingService = service;
+		}
+
+		[HttpGet("all")]
+		public async Task<ActionResult<ApiResponse<IEnumerable<BookingDto>>>> GetAllBookings(CancellationToken ct)
+		{
+			var result = await _bookingService.GetAllAsync(ct);
+			if (result.IsError)
+			{
+				return BadRequest(result);
+			}
+
+			return Ok(result);
+		}
+
+	}
+}

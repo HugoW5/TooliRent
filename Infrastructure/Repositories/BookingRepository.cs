e4f7cc931512a4562ecf6b1e5118ba0f62b3059c
HugoW5 <hugo05.wakt@gmail.com>
2025-09-10 16:23:28 +0200

    Add Async

---
+++ Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Models;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+	public class BookingRepository
+	{
+		private readonly ApplicationDbContext _context;
+
+		public BookingRepository(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<Guid?> AddAsync(Booking booking, CancellationToken ct = default)
+		{
+			await _context.Bookings.AddAsync(booking, ct);
+			await _context.SaveChangesAsync(ct);
+			return booking.Id;
+		}
+	}
+}

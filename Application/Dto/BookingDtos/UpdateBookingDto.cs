f87b02a28481b83b409edf65b13819649ca6ff98
HugoW5 <hugo05.wakt@gmail.com>
2025-09-10 16:32:06 +0200

    Add UpdateBookingDto class for booking updates
    Introduces the `UpdateBookingDto` class in the
    `Application.Dto.BookingDtos` namespace. This class
    includes properties for `StartAt`, `EndAt`, and
    `Status`, along with necessary using directives
    for proper functionality.

---
+++ Application/Dto/BookingDtos/UpdateBookingDto.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dto.BookingDtos
+{
+
+	public class UpdateBookingDto
+	{
+		public DateTime StartAt { get; set; }
+		public DateTime EndAt { get; set; }
+		public BookingStatus Status { get; set; }
+	}
+}
